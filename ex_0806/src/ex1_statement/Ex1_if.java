65ac6b7b8dd985e68a2eab75a354f107f51acce2
gibum1008 <gibum1008@gmail.com>
2026-08-06 14:52:40 +0900

    0806 수업내용

--- ex_0806/src/ex1_statement/Ex1_if.java
+++ ex_0806/src/ex1_statement/Ex1_if.java
@@ -3,8 +3,25 @@ package ex1_statement;
 public class Ex1_if {
     public static void main(String[] args) {
         
-        System.out.println("안녕");
-        
+       //제어문 - 프로그램의 흐름을 제어하는 문장
+       //분기문 , 반복문 나뉨
+       // 분기문 : if ,switch
+       //반복문 : for, while, do-while
+
+       //if문
+       //if(조건식){
+       // 조건식이 참일 떄 실행되는 영역
+       //}
+
+        int n =50;
+        String str ="안녕하세요"; //String ""안에 (문자열)넣을수 있는 자료형임
+
+        if(n != 50 ){
+            str = "n은 50입니다";
+
+        }
+
+        System.out.println(str);
 
 
     }//main
